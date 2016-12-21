6f8d92ac3502f0da50015289ed81efa228adbfd3
jcraymond <raymond.jacob17@gmail.com>
2016-12-21 17:51:32 -0500

    TODO: initialize Force with access to the objects within the Engine

--- engine/src/main/java/bynarie/engine/Force.java
+++ engine/src/main/java/bynarie/engine/Force.java
@@ -3,6 +3,7 @@ package bynarie.engine;
 import bynarie.math.Vector;
 
 public abstract class Force {
+    //TODO initialize with access to all of the objects in the Engine
     public abstract Vector GetForce(PhysicsObject po);
 
     void ApplyForce(PhysicsObject po) {
